2274feb7ad08cfe68ce3a46ad0f0f8d78cbfbe75
Venkat Sykam <venkatsykam1289@gmail.com>
2022-08-27 02:20:52 -0500

    Added a couple login page elements

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,18 @@ import './Login.css'
 function Login() {
   return (
     <div className='login'>
-        
+        <div className="login__container">
+            <h1>Sign into your MyDelicacy account</h1>
+            <form className='login__info'>
+                <input type="text" placeholder='Email...'/>
+                <input type="text" placeholder='Password...'/>
+                <button className='login__signInButton'>Sign in</button>
+            </form>
+
+            <p className="login__forgotPassword">Forgot password?</p>
+            <button className='login__createAccount'>Create your MyDelicacy Account</button>
+
+        </div>
     </div>
   )
 }
